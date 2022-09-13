2e5a92bc9f5baa21c415aef18739c5a347b2e4a0
Lique007 <angeliquehilario019@gmail.com>
2022-09-13 08:35:41 -0700

    tasks 0-6

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Square file
+"""
+
+
+class Square:
+    """square class"""
+    pass
+
+    def __init__(self, size):
+        """
+         init method
+         :param size: size of the Square
+         Args:
+             size: size of the Square
+         """
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+a project about classes and objects
