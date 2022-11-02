9accbd1854387963c885e4222a00a08961f73349
Lique007 <angeliquehilario019@gmail.com>
2022-11-02 12:11:00 -0700

    list all records of the second table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- List all records in second_table 
+SELECT *  score,name(ASC) FROM second_table; 	
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- create a table
+-- create a tablea and add multiple rows 
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
